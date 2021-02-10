52745bee6cd89bc51f690c4baa8dfd2c2ddcc6c3
Jon <>
2021-02-10 16:08:41 -0500

    Add working leaflet open street map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit, OnInit {
+  private map;
+
+  private initMap(): void {
+    this.map = L.map('map').setView([39.8282, -98.5795], 3);
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
   }
 
 }
