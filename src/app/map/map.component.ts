4cf5f7d86506690746c21590a963d77faa872821
Jon <>
2021-02-11 16:34:50 -0500

    Refactor fetchLocations and initMap methods

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -12,25 +12,17 @@ import { MapService } from './map.service'
 export class MapComponent implements AfterViewInit, OnInit {
 
   private map;
-  heat;
-  circle;
-  locations;
-  heatMapPoints = [];
+  heatMapPoints;
 
   private initMap(): void {
     this.map = L.map('map').setView([14.2844133, 102.2565852], 3);
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-    tiles.addTo(this.map);
+    }).addTo(this.map);
 
-    this.locations.forEach(element => {
-      this.heatMapPoints.push([element.latitude, element.longitude]);
-    });
-
-    this.heat = new L.heatLayer(this.heatMapPoints, {
+    L.heatLayer(this.heatMapPoints, {
       radius: 11,
       minOpacity: 0.4,
       gradient: {0.4: 'blue', 0.5: 'lime', 0.6: 'red'}
@@ -40,7 +32,7 @@ export class MapComponent implements AfterViewInit, OnInit {
   constructor(private mapService: MapService) { }
 
   ngOnInit() {
-    this.locations = this.mapService.getLocations();
+    this.heatMapPoints = this.mapService.getLocations();
   }
 
   ngAfterViewInit() {
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 import { MapService } from '../map/map.service';
 
@@ -14,10 +14,19 @@ export class DataStorageService {
 
   fetchLocations() {
     return this.http
-      .get(
+      .get<Array<any>>(
         'http://localhost:3000/iplocations'
       )
       .pipe(
+        map(iplocations => {
+          let locations = [];
+          iplocations.forEach(location => {
+            if (location.latitude && location.longitude) {
+              locations.push([location.latitude, location.longitude]);
+            }
+          })
+          return locations;
+        }),
         tap(locations => {
           this.mapService.setLocations(locations);
         })
