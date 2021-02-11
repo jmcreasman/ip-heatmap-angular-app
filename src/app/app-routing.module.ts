ac4aa447f61059b008b4463b04c89b4e98a0053e
Jon <>
2021-02-11 11:35:10 -0500

    Add communication from frontend to backend for requesting and storing gps locations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapResolverService } from './map/map-resolver.service';
 import { MapComponent } from './map/map.component';
 
 const routes: Routes = [
-  { path: '', component: MapComponent}
+  { path: '', component: MapComponent, resolve: [MapResolverService]}
 ];
 
 @NgModule({
